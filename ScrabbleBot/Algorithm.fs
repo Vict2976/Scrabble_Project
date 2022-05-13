ddd69b48783ae1ba24a6103ded0eac8c407ee9ad
niwl <niwl@itu.dk>
2022-05-13 11:19:44 +0200

    algorightm.fs

---
+++ ScrabbleBot/Algorithm.fs
@@ -0,0 +1,14 @@
+namespace NiwlSamdVibr
+open System
+open MultiSet
+open ScrabbleUtil
+open ScrabbleUtil.ServerCommunication
+
+open System.IO
+
+open ScrabbleUtil.DebugPrint
+open StateMonad
+
+
+
+
